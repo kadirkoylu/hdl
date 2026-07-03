1cd036cecbc0e0802885c04bafd6ecb9318be211
Elena-Hadarau_adi <Elena.Hadarau@analog.com>
2026-07-03 16:32:18 +0300

    xcvr_wizard: Extend for jesd204c
    Signed-off-by: Elena-Hadarau_adi <Elena.Hadarau@analog.com>

--- projects/xcvr_wizard/kc705/system_bd.tcl
+++ projects/xcvr_wizard/kc705/system_bd.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-## Copyright (C) 2025 Analog Devices, Inc. All rights reserved.
+## Copyright (C) 2025-2026 Analog Devices, Inc. All rights reserved.
 ### SPDX short identifier: ADIBSD
 ###############################################################################
 
@@ -7,5 +7,13 @@ set LANE_RATE $ad_project_params(LANE_RATE)   ; # [gbps]
 set PLL_TYPE  $ad_project_params(PLL_TYPE)    ; # [CPLL/QPLL]
 set REF_CLK   $ad_project_params(REF_CLK)     ; # [MHz]
 
+foreach rx_param {XCVR_RX_PLL_TYPE XCVR_RX_LANE_RATE XCVR_RX_REF_CLK} {
+    if {[info exists ad_project_params($rx_param)] && $ad_project_params($rx_param) ne ""} {
+        set $rx_param $ad_project_params($rx_param)
+    } else {
+        set $rx_param ""
+    }
+}
+
 source $ad_hdl_dir/projects/scripts/gtwizard_generator.tcl
-get_diff_params $LANE_RATE $PLL_TYPE $REF_CLK
+get_diff_params $LANE_RATE $PLL_TYPE $REF_CLK $XCVR_RX_PLL_TYPE $XCVR_RX_LANE_RATE $XCVR_RX_REF_CLK
